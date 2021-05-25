2174a4a79d2ae4c8dd81b3acfa7a2033877e339c
Geovany Dutan <geovannydutanm@gmail.com>
2021-05-25 23:15:50 +0200

    ADD CODE ADS_L3-7
    ADD CODE ADS_L3-7

---
+++ MapeK.Probe.SensorFailurePlan/src/mapek/probe/sensorfailureplan/Listener.java
@@ -0,0 +1,49 @@
+package mapek.probe.sensorfailureplan;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceEvent;
+import org.osgi.framework.ServiceListener;
+
+import mapek.interfaces.IMonitor;
+import sua.autonomouscar.devices.interfaces.IDistanceSensor;
+import sua.autonomouscar.devices.interfaces.ILineSensor;
+import sua.autonomouscar.devices.interfaces.IRoadSensor;
+import sua.autonomouscar.infrastructure.OSGiUtils;
+import sua.autonomouscar.interfaces.IIdentifiable;
+import sua.autonomouscar.mapek.infrastructure.monitors.MonitorVehicleContext;
+
+
+public class Listener implements ServiceListener{
+	protected BundleContext context = null;
+	
+	public Listener(BundleContext context) {
+		this.context=context;
+	}
+	
+	@Override
+	public void serviceChanged(ServiceEvent event) {
+		//System.out.println("Detected change: "+event.getType());
+
+		ILineSensor rightLineSensor=OSGiUtils.getService(context, ILineSensor.class, String.format("(%s=%s)", IIdentifiable.ID, "RightLineSensor"));
+		IDistanceSensor rightDistanceSensor =	OSGiUtils.getService(context, IDistanceSensor.class, String.format("(%s=%s)", IIdentifiable.ID, "RightDistanceSensor"));
+
+		switch (event.getType()) {
+			case ServiceEvent.MODIFIED_ENDMATCH:
+			case ServiceEvent.UNREGISTERING:
+			case ServiceEvent.REGISTERED:
+			case ServiceEvent.MODIFIED:
+				 MonitorVehicleContext monitorVehicleContext =  (MonitorVehicleContext) OSGiUtils.getService(context, IMonitor.class, String.format("(%s=%s)", IIdentifiable.ID, "MonitorVehicleContext"));
+
+				if(rightLineSensor==null||rightDistanceSensor==null) {
+					//Actualizar el monitor a que NO se puede realizar el cambio/remplazo 
+					monitorVehicleContext.changedState(false);
+				}else {
+					//Actualizar el monitor a que SI se puede realizar el cambio/remplazo 
+					monitorVehicleContext.changedState(true);
+
+				}
+			
+		}
+		
+	}
+}
